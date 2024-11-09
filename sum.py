f240e93f93e1d0bd173f0bafd1dc4bbb1888bfdc
nakahara <OCA2204240019@edu.oca.ac.jp>
2024-11-09 14:20:14 +0900

    演習まで完了

---
+++ sum.py
@@ -0,0 +1,9 @@
+def sum():
+    return 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10
+
+
+def sum1():
+    result = 0
+    for i in range(1, 11):
+        result += i
+    return result
---
+++ test_sum.py
@@ -0,0 +1,7 @@
+from sum import sum1
+
+
+def test_sum1():
+    result = sum1()
+
+    assert result == 55
